f314b7beec56d189c359356fbdebed1dc3032b8c
LanJiang823 <ljia825@aucklanduni.ac.nz>
2015-08-14 16:41:07 +1200

    [#013] test by Lan

--- CognitionMonitor/src/com/warriortech/cognitionmonitor/FullscreenActivity.java
+++ CognitionMonitor/src/com/warriortech/cognitionmonitor/FullscreenActivity.java
@@ -121,7 +121,7 @@ public class FullscreenActivity extends Activity {
 		findViewById(R.id.dummy_button).setOnTouchListener(
 				mDelayHideTouchListener);
 		
-		// Load the html game stored in the assets, gracy please help us!!!
+		// Load the html game stored in the assets
 		mainWebView = (WebView) findViewById(R.id.myWebView);
 		mainWebView.getSettings().setJavaScriptEnabled(true);
 		mainWebView.setWebChromeClient(new WebChromeClient());
