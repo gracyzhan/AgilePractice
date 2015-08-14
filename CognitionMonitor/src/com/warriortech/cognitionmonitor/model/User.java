d2db95d8a0a79410dc37e959e4ab57c5b5319792
Rli685 <jerry@onlinesale.co.nz>
2015-08-14 16:55:54 +1200

    [#015] Add temp class for model class "User"

---
+++ CognitionMonitor/src/com/warriortech/cognitionmonitor/model/User.java
@@ -0,0 +1,18 @@
+package com.warriortech.cognitionmonitor.model;
+
+public class User {
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	private String name;
+	private int ID;
+}
