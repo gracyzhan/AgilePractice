192090752536016d8378b39d2f0b81f6a5b83ae9
Rli685 <jerry@onlinesale.co.nz>
2015-08-19 11:02:42 +1200

    [#033] Save the fonts css to local, add few new unit test

--- TestCognitionMonitor/src/com/warriortech/cognitionmonitor/test/testStoreScore.java
+++ TestCognitionMonitor/src/com/warriortech/cognitionmonitor/test/testStoreScore.java
@@ -3,10 +3,16 @@ package com.warriortech.cognitionmonitor.test;
 import java.util.Date;
 import java.util.List;
 
+import org.hamcrest.core.IsNull;
+
 import com.warriortech.cognitionmonitor.FullscreenActivity;
 import com.warriortech.cognitionmonitor.TestDao;
 import com.warriortech.cognitionmonitor.dao.PlayScoresHis;
 import com.warriortech.cognitionmonitor.dao.PlayScoresHisDaoImpl;
+import com.warriortech.cognitionmonitor.model.GameResult;
+import com.warriortech.cognitionmonitor.model.User;
+import com.warriortech.cognitionmonitor.service.Impl.GameStateControllerImpl;
+import com.warriortech.cognitionmonitor.service.Interface.GameStateController;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -24,9 +30,44 @@ public class testStoreScore extends ActivityInstrumentationTestCase2<FullscreenA
 	public void testActivityExists() {
 		FullscreenActivity activity = getActivity();
 	    assertNotNull(activity);
+	    
 	}
 	
-	public void testGreet() {
+	public void testSaveGameState() {
+		FullscreenActivity activity = getActivity();
+		Context context=activity.getApplicationContext();
+		PlayScoresHisDaoImpl playScoresHisDaoImpl = new PlayScoresHisDaoImpl( context );
+		
+		GameStateController gsController=new GameStateControllerImpl();
+		GameResult gr=new GameResult("TestGame101", "9999", "100023");
+		User user=new User("Test", "User", "11111111", "555");
+		
+		gsController.saveAbilityTestResult(context, user, gr);
+		
+		PlayScoresHis playScoresHis = new PlayScoresHis();
+		playScoresHis.setPlayGameId("TestGame101");
+		Cursor cursor = playScoresHisDaoImpl.GetCursorByGameId(playScoresHis) ;
+		if ( cursor.getCount() <1)
+		{
+			// error 
+		}
+		assertTrue("Not insert successful", cursor.getCount()>0);
+		
+		
+		
+		List<PlayScoresHis> playScoresHisArray =  playScoresHisDaoImpl.ConvertCursorToEntityClass(cursor) ;
+		PlayScoresHis playScoreHis2=playScoresHisArray.get(0);
+		assertTrue("Can Not get the record out", playScoreHis2!=null);
+		assertEquals("Get the incorrect play time", playScoreHis2.getPlayTime(), "100023");
+		assertEquals("Get the incorrect game id", playScoreHis2.getPlayGameId(), "TestGame101");
+		assertEquals("Get the incorrect gamename", playScoreHis2.getPlayGameName(), "TestGame101");
+
+		assertEquals("Get the incorrect score", playScoreHis2.getScore(), "9999");
+		
+    }
+	
+	
+	public void testDAO() {
 		FullscreenActivity activity = getActivity();
 		Context context=activity.getApplicationContext();
 		PlayScoresHisDaoImpl playScoresHisDaoImpl = new PlayScoresHisDaoImpl( context );
@@ -45,19 +86,16 @@ public class testStoreScore extends ActivityInstrumentationTestCase2<FullscreenA
 		{
 			// error 
 		}
+		assertTrue("Not insert successful", cursor.getCount()>0);
+		
+		
+		
 		List<PlayScoresHis> playScoresHisArray =  playScoresHisDaoImpl.ConvertCursorToEntityClass(cursor) ;
-		if ( playScoresHisArray != null && playScoresHisArray.size() >0)
-		{
-			 for ( int i =0 ; i<playScoresHisArray.size() ;i++)
-			 {
-				 System.out.println( playScoresHisArray.get(i).getID() );  
-				 System.out.println( playScoresHisArray.get(i).getMachineId() );  
-				 System.out.println( playScoresHisArray.get(i).getPlayGameId());  
-				 System.out.println( playScoresHisArray.get(i).getPlayGameName() );  
-				 System.out.println( playScoresHisArray.get(i).getPlayTime() );  
-				 System.out.println( playScoresHisArray.get(i).getScore() ); 
-				 
-			 }
-		}
+		PlayScoresHis playScoreHis2=playScoresHisArray.get(0);
+		assertTrue("Can Not get the record out", playScoreHis2!=null);
+		assertEquals("Get the incorrect machine id", playScoreHis2.getMachineId(), "gracyMac");
+		assertEquals("Get the incorrect game id", playScoreHis2.getPlayGameId(), "123");
+		assertEquals("Get the incorrect gamename", playScoreHis2.getPlayGameName(), "test1");
+		
     }
 }
