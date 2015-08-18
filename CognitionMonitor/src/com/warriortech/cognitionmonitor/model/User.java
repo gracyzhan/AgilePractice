10b0e9017b3743df504d312d41c9beed14de4383
ximin Yan <harryyan1238@gmail.com>
2015-08-18 18:21:20 +1200

    [#030] modify the user model

--- CognitionMonitor/src/com/warriortech/cognitionmonitor/model/User.java
+++ CognitionMonitor/src/com/warriortech/cognitionmonitor/model/User.java
@@ -41,8 +41,84 @@ public class User {
 		this.userId = id;
 	}
 
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the birthDate
+	 */
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	/**
+	 * @param birthDate the birthDate to set
+	 */
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public int getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the mobileNum
+	 */
+	public String getMobileNum() {
+		return mobileNum;
+	}
+
+	/**
+	 * @param mobileNum the mobileNum to set
+	 */
+	public void setMobileNum(String mobileNum) {
+		this.mobileNum = mobileNum;
+	}
+
 	private String firstName;
 	private String lastName;
+	private String userName;
 	private String password;
 	private String userId;
+	private String email;
+	private String birthDate;
+	private String mobileNum;
+	private int gender;
+	
 }
